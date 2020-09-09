def3df3128b5c57234564c346a4622946c037979
Tom Coupe <thomas.coupe@yahoo.co.uk>
2020-09-09 15:31:07 +0100

    Particle affect completed. UI elements need adding

--- script.js
+++ script.js
@@ -5,9 +5,9 @@ canvas.height = window.innerHeight;
 
 let particlesArray;
 let mouse = {
-    x = null,
-    y = null,
-    radius: (canvas.height / 80) * (canvas.width / 80)
+    x: null,
+    y: null,
+    radius: (canvas.height / 100) * (canvas.width / 100)
 }
 
 window.addEventListener("mousemove",
@@ -30,9 +30,92 @@ class Circle {
     draw() {
         ctx.beginPath();
         ctx.arc(this.x, this.y, this.size, 0, Math.PI * 2, false);
+        ctx.fillStyle = '#5c5c5c';
+        ctx.fill();
     }
 
     update() {
+        if(this.x > canvas.width || this.x < 0) {
+            this.directionX = -this.directionX;
+        }
 
+        if(this.y > canvas.height || this.y < 0) {
+            this.directionY = -this.directionY;
+        }
+
+        let dx = mouse.x - this.x;
+        let dy = mouse.y - this.y;
+
+        let distance = Math.sqrt(dx * dx + dy * dy);
+
+        if(distance < mouse.radius + this.size) {
+            if(mouse.x < this.x && this.x < canvas.width - this.size * 10) {
+                this.x += 10;
+            }
+            if(mouse.x > this.x && this.x > this.size * 10) {
+                this.x -= 10;
+            }
+            if(mouse.y < this.y && this.y < canvas.height - this.size * 10) {
+                this.y += 10;
+            }
+            if(mouse.y > this.y && this.y > this.size * 10) {
+                this.y -= 10;
+            }
+        }
+
+        this.x += this.directionX;
+        this.y += this.directionY;
+
+        this.draw();
     }
 }
+
+function init() {
+    particlesArray = [];
+    let numberOfParticles = (canvas.height * canvas.width) / 8000;
+    for(let i = 0; i < numberOfParticles; i++) {
+        let size = (Math.random() * 5) + 1;
+        let x = (Math.random() * ((innerWidth - size * 2) - (size * 2)) + size * 2);
+        let y = (Math.random() * ((innerHeight - size * 2) - (size * 2)) + size * 2);
+        let directionX = (Math.random() * 5) - 2.5;
+        let directionY = (Math.random() * 5) - 2.5;
+        let color = '#5c5c5c';
+
+        particlesArray.push(new Circle(x, y, directionX, directionY, size, color));
+    }
+}
+
+function connect() {
+    for(let a = 0; a < particlesArray.length; a++) {
+        for(let b = a; b < particlesArray.length; b++) {
+
+            let distance = (( particlesArray[a].x - particlesArray[b].x)
+             * (particlesArray[a].x - particlesArray[b].x))
+             + ((particlesArray[a].y - particlesArray[b].y) * 
+             (particlesArray[a].y - particlesArray[b].y));
+
+            if (distance < (canvas.width/7) * (canvas.height/7)) {
+                ctx.strokeStyle = 'rgb(169,169,169)';
+                ctx.lineWidth = 2;
+                ctx.beginPath();
+                ctx.moveTo(particlesArray[a].x, particlesArray[a].y);
+                ctx.lineTo(particlesArray[b].x, particlesArray[b].y);
+                ctx.stroke();
+            }
+        }
+    }
+}
+
+function animate() {
+    requestAnimationFrame(animate);
+    ctx.clearRect(0, 0, innerWidth, innerHeight);
+
+    for(let i = 0; i < particlesArray.length; i++) {
+        particlesArray[i].update();
+    }
+    connect();
+}
+
+
+init();
+animate();
